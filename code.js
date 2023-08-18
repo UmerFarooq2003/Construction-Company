289b1ed3770e96ec23878af217256415f216f5bf
Umer Farooq <135502597+UmerFarooq2003@users.noreply.github.com>
2023-08-18 09:54:33 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,19 @@
+function openContactUs() {
+    window.open('ContactUs/UnitedEngineeringAssociates.html', '_blank');
+}
+function openAboutUs(){
+    window.open('AboutUs/U.E.A.html','_blank');
+}
+function openServices(){
+    window.open('Services/papa.html','_blank');
+}
+function openPortfolio(){
+    window.open('Portfolio/Experience.html','_blank');
+}
+function openProject(){
+    window.open('project/Construct.html','_blank');
+}
+function toggleNavbarMenu() {
+    var navbarMenu = document.getElementById('navbarMenu');
+    navbarMenu.classList.toggle('show');
+}
